f0095fd62e2a839ab5f59de0dd4debd2300b7b01
anshu <anshhufx@gmail.com>
2026-05-24 10:50:00 +0530

    work w/ spacing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import { ChevronRight, ChevronLeft } from "lucide-react";
+import { ChevronRight, ChevronLeft, EllipsisVertical } from "lucide-react";
 
 const App = () => {
   return (
-    <div className="mt-32 flex justify-center items-center">
-      <div className="h-[21rem] w-[22rem] bg-stone-100 rounded-3xl border-2 border-neutral-400/30 px-7 py-5">
+    <div className="mt-32 flex flex-col gap-2 justify-center items-center">
+      <div className="h-[21rem] w-[22rem] bg-stone-100 rounded-3xl border-2 border-neutral-400/30 px-7 py-5 cursor-pointer active:scale-95 transition-transform duration-200">
         {/* Header */}
         <div className="flex font-bold justify-between items-center">
           <ChevronLeft />
@@ -59,6 +59,26 @@ const App = () => {
           <button className="text-neutral-400/80">1</button>
           <button className="text-neutral-400/80">2</button>
           <button className="text-neutral-400/80">3</button>
+        </div>
+      </div>
+      <div className="h-[5rem] w-[22rem] bg-stone-100 rounded-3xl border-2 border-neutral-400/30 px-5 py-5 flex justify-between cursor-pointer active:scale-95 transition-transform duration-200">
+        <div className="flex justify-center items-center gap-4">
+          <div className="w-10">
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/28/Apple_TV_Plus_Logo.svg/1280px-Apple_TV_Plus_Logo.svg.png"
+              alt=""
+            />
+          </div>
+          <div>
+            <p className="text-sm font-bold tracking-tight">Apple TV+</p>
+            <p className="text-xs text-neutral-400 tracking-tight">
+              15 Feb, 2024
+            </p>
+          </div>
+        </div>
+        <div className="flex justify-center items-center gap-2">
+          <p className="font-semibold tracking-tight text-[14px]">6,99 EUR</p>
+          <EllipsisVertical size={18} />
         </div>
       </div>
     </div>
